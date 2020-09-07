f0b6660f73d885f520183f2692ddd20c3a432830
xpenalosa <xavier.penalosa.esteller@gmail.com>
2020-09-07 20:41:54 +0200

    Remove unnecessary implements

--- src/main/java/com/learn_spring/demo/Root.java
+++ src/main/java/com/learn_spring/demo/Root.java
@@ -27,10 +27,9 @@ package com.learn_spring.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class Root implements WebMvcConfigurer {
+public class Root {
 
     @GetMapping("/")
     public String root(){
