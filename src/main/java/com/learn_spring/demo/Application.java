fa5ea604ae648805a270c73c4b36d995af62cc54
xpenalosa <xavier.penalosa.esteller@gmail.com>
2020-09-07 23:12:01 +0200

    Remove unused imports

--- src/main/java/com/learn_spring/demo/Application.java
+++ src/main/java/com/learn_spring/demo/Application.java
@@ -35,11 +35,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.concurrent.Executor;
 
 
 @SpringBootApplication
